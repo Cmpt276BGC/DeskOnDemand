5ac1e6ac4b86dab2d798dc0c9773a23504a4d143
Matt Sass <matthew.sass35@gmail.com>
2022-02-27 12:44:41 -0800

    Matts test commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //Patrick Pulled / pushed successfully 
 // harsh's commit
+//Matt's commit
 
 const express = require('express')
 const path = require('path')
@@ -10,4 +11,5 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/db', (req, res) => res.render('pages/db'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
