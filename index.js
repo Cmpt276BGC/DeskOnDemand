cd2b1cf60c9b128ef3c49215c63e211fe7ee8b75
Bhavneet Singh <bsr1@sfu.ca>
2022-03-01 08:40:15 -0800

    styling the pages

--- index.js
+++ index.js
@@ -11,17 +11,18 @@ const session = require('express-session')
 
 var app = express()
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(session({
+ app.use(session({
   name: "sesson",
   secret: 'this is the way', 
   resave: false, // forces session to be saved back to session store
   saveUninitialized: false, // forces a session that is "uninitialized" to be saved to store
   maxAge: 30 * 60 * 1000, // 30 minutes
-}))
+})) 
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 app.get('/login', (req, res) => {
     res.render('pages/loginPage')
@@ -31,4 +32,6 @@ app.get('/register', (req, res) => {
   res.render('pages/registerPage')
 })
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/dashboard', (req,res)=>{
+  res.render('pages/dashboard')
+})
