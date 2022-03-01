9a6a1bfd0e570cec30a907439e975aa811201bd0
Emmii Cui <emmiic@sfu.ca>
2022-02-28 22:17:56 -0800

    testlogin and added express-session

--- index.js
+++ index.js
@@ -2,22 +2,33 @@
 // harsh's commit
 //Matt's commit
 
+// emmii's commit 02-28-2022
+
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const session = require('express-session')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+var app = express()
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(session({
+  name: "sesson",
+  secret: 'this is the way', 
+  resave: false, // forces session to be saved back to session store
+  saveUninitialized: false, // forces a session that is "uninitialized" to be saved to store
+  maxAge: 30 * 60 * 1000, // 30 minutes
+}))
 
-  .get('/login', (req, res) => {
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+
+app.get('/login', (req, res) => {
     res.render('pages/loginPage')
-  })
+})
 
-  .get('/register', (req, res) => {
-    res.render('pages/registerPage')
-  })
+app.get('/register', (req, res) => {
+  res.render('pages/registerPage')
+})
 
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
