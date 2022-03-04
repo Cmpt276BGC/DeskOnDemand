a036a568754200d9237d62f206752262c0870e3d
Emmii Cui <emmiic@sfu.ca>
2022-03-04 15:47:24 -0800

    added table 'BGCUsers' to online database

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/dashboard', (req,res)=>{
 app.get('/db', async (req, res) => {
   try {
     const client = await pool.connect();
-    const result = await client.query('SELECT * FROM test_table');
+    const result = await client.query('SELECT * FROM BGCUsers');
     const results = { 'results': (result) ? result.rows : null};
     res.render('pages/db', results );
     client.release();
