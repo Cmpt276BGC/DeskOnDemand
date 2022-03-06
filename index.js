919189754d1a96e59a9cc971392c702d5ee41852
v3coding <59391766+v3coding@users.noreply.github.com>
2022-03-06 01:57:58 -0800

    heroku testing / devlopment (done with go.bat) by typing ./go in the root directory

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.post('/register', async (req, res) => {
 
     //var existsQueryResult = {'existQueryResult' : existsQuery.rows[0].exists};
 
-    res.send(existsQuery);
+    //res.send(existsQuery);
 
     if(existsQuery.rows[0].exists){
       res.send("email already in use");
