67cc264f0caa14005b4fa791f84cb554c2715b25
v3coding <59391766+v3coding@users.noreply.github.com>
2022-03-05 23:07:41 -0800

    heroku testing / devlopment (done with go.bat) by typing ./go in the root directory

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.get('/tokenDump', (req,res)=>{
 
   //var adminQueryString = JSON.stringify(req.session.users.rows[0].admin);
 
-  res.send(req.session.user.rows[0].admin);
+  res.send(req.session.user.rows[0].admin[0]);
   
 })
 
