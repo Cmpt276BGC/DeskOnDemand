c20b3cc3ca252d371a17a09cac87d48cee22f3cd
matthew-sass <matthew.sass35@gmail.com>
2022-03-10 00:34:31 -0800

    Forgot to set connectionstring back to heroku db

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ const { user } = require('pg/lib/defaults');
 const { error } = require('console');
 var pool;
 pool = new Pool({
-  //connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
   //connectionString: 'postgres://postgres:1433@localhost/bgc',  // emmii's local database
-  connectionString: 'postgres://postgres:Jojek2020.@localhost/dod', //matts local db
-  //ssl: {
-  //  rejectUnauthorized: false
-  //}
+  //connectionString: 'postgres://postgres:Jojek2020.@localhost/dod', //matts local db
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 var app = express()
