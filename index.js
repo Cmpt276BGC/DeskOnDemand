05b99396843ddf8458e53a8a888ace6521a4c865
Emmii Cui <emmiic@sfu.ca>
2022-03-22 21:14:33 -0700

    added /db for displaying bgcusers table data for troubleshooting

--- index.js
+++ index.js
@@ -176,7 +176,19 @@ function checkAuthorization(req, res, next) {
   return res.redirect('/users/dashboard');
 }
 
-
+// for troubleshooting purposes - displays bgcusers db data
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM BGCUsers');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
 
 // emmii's note-to-self: DO NOT DELETE BEYOND THIS POINT!!!!!!
 
