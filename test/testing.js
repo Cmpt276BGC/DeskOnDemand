7989ded1522b85bd5bb371aa4ea807d8b561faf4
Harsh <harshjot.ss1@gmail.com>
2022-03-22 22:20:13 -0700

    updated tests to work with new login

--- test/testing.js
+++ test/testing.js
@@ -6,7 +6,7 @@ var should = chai.should()
 const { Pool } = require('pg');
 var pool = new Pool({
 //   connectionString: 'postgres://postgres:root@localhost/bgcusers'
-  connectionString: 'postgres://postgres:Jojek2020.@localhost/dod' //Matts connection string
+//   connectionString: 'postgres://postgres:Jojek2020.@localhost/dod' //Matts connection string
 });
 
 chai.use(chaiHttp)
@@ -14,26 +14,26 @@ chai.use(require('chai-json'))
 chai.use(require('chai-things'))
 
 before(function() {
-    pool.query(`delete from bgcusers where upass='test1234'`)
+    pool.query(`delete from bgcusers where fname='test'`)
 })
 
 describe('Register', function() {
-    pool.query(`delete from bgcusers where upass='test1234'`)
+    pool.query(`delete from bgcusers where fname='test'`)
 
     it('should register a new account', function(done) {
-        chai.request(server).post('/register')
+        chai.request(server).post('/users/register')
             .send({
-                firstNameInput: "test",
-                lastNameInput: "test",
-                emailInput: "test@test.com",
-                passwordInput: "test1234",
-                confirmPasswordInput: "test1234"
+                fname: "test",
+                lname: "test",
+                email: "test@test.com",
+                password: "test1234",
+                confirmpw: "test1234"
             })
             .redirects(0)
             .end(function(err, res) {
                 should.not.exist(err)
                 res.should.have.status(302)
-                res.should.redirectTo('/login')
+                res.should.redirectTo('/users/login')
                 res.should.be.text
                 res.body.should.be.a('object')
                 done()
@@ -42,60 +42,95 @@ describe('Register', function() {
 })
 
 describe('Login', function() {
-    it('should successfully login as a normal user and redirect to /userPage', function(done) {
-        chai.request(server).post('/login')
+    it('should successfully login as a normal user and redirect to /users/login', function(done) {
+        chai.request(server).post('/users/login')
             .send({
-                userEmailInput: 'test@test.com',
-                userPasswordInput: 'test1234'
+                email: 'test@test.com',
+                password: 'test1234'
             })
             .redirects(0)
             .end(function(err, res) {
                 should.not.exist(err)
                 res.should.have.status(302)
-                res.should.redirectTo('/userPage')
+                res.should.redirectTo('/users/dashboard')
                 res.should.be.text
                 res.body.should.be.a('object')
+                done()
             })
     })
 
-    it('should successfully login as an admin user and redirect to /adminPage', function(done) {
-        chai.request(server).post('/login')
+    it('should not login when given an incorrect email/password', function(done) {
+        chai.request(server).post('/users/login')
             .send({
-                userEmailInput: 'admin@admin.com',
-                userPasswordInput: 'admin123'
+                email: 'testdoesnotexist@test.com',
+                password: 'test1234'
             })
             .redirects(0)
             .end(function(err, res) {
                 should.not.exist(err)
                 res.should.have.status(302)
-                res.should.redirectTo('/adminPage')
+                res.should.redirectTo('/users/login')
                 res.should.be.text
-                res.should.be.a('object')
+                res.body.should.be.a('object')
+                done()
+            })
+    })
+
+    it('should successfully login as a admin user and redirect to /users/admindash', function(done) {
+        chai.request(server).post('/users/adminlogin')
+            .send({
+                email: 'admin@admin.com',
+                password: 'admintest'
+            })
+            .redirects(0)
+            .end(function(err, res) {
+                should.not.exist(err)
+                res.should.have.status(302)
+                res.should.redirectTo('/users/admindash')
+                res.should.be.text
+                res.body.should.be.a('object')
+                done()
+            })
+    })
+
+    it('should not login when given an incorrect admin email/password', function(done) {
+        chai.request(server).post('/users/adminlogin')
+            .send({
+                email: 'admindoesnotexist@admin.com',
+                password: 'admintest'
+            })
+            .redirects(0)
+            .end(function(err, res) {
+                should.not.exist(err)
+                res.should.have.status(302)
+                res.should.redirectTo('/users/adminlogin')
+                res.should.be.text
+                res.body.should.be.a('object')
                 done()
             })
     })
 })
 
-//not complete
-describe('Search', function(){
-    it('should search for available workstations on a particular date with no specific floor or attributes', function(done){
-        chai.request(server).post('/searchTablesSpecificDate')
-            .send({
-                specificDateISOString: '2022-03-16',
-                specificDateEndISOString: '2022-03-17',
-                floor: 'any',
-                office: 'false',
-                window: 'false',
-                corner: 'false',
-                cubicle: 'false',
-                single: 'false',
-                double: 'false'
-            })
-            .end(function(err, res){
-                should.not.exist(err)
-                res.should.have.status(200)
-                res.should.be.json
-                done()
-            })
-    })
-})
+// //not complete
+// describe('Search', function(){
+//     it('should search for available workstations on a particular date with no specific floor or attributes', function(done){
+//         chai.request(server).post('/searchTablesSpecificDate')
+//             .send({
+//                 specificDateISOString: '2022-03-16',
+//                 specificDateEndISOString: '2022-03-17',
+//                 floor: 'any',
+//                 office: 'false',
+//                 window: 'false',
+//                 corner: 'false',
+//                 cubicle: 'false',
+//                 single: 'false',
+//                 double: 'false'
+//             })
+//             .end(function(err, res){
+//                 should.not.exist(err)
+//                 res.should.have.status(200)
+//                 res.should.be.json
+//                 done()
+//             })
+//     })
+// })
