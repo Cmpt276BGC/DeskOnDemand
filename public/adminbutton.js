77c4d3c5159a2a5d56773452bb55dd25728dc090
Emmii Cui <emmiic@sfu.ca>
2022-03-24 16:08:04 -0700

    deletion of debugging codes that was printing to html page

--- public/adminbutton.js
+++ public/adminbutton.js
@@ -1,25 +1,20 @@
 var radios = document.forms["radioform"].elements["title"];
 var IDtoSend = document.getElementById("IDtoSend");
 
-function updateIDtoSend() {
-  IDtoSend.value = selectedID.value;
-  console.log(IDtoSend.value);
-}
-
 // admin booking only enabled for admins
 function adminButton(){
   const isAuthorized = document.getElementById("authorized").value;
-  console.log(isAuthorized);
+ 
   if (isAuthorized != "true") {
     document.getElementById("adminButton").disabled = true;
   }
 };
+
 for (var i = 0; i<radios.length; i++) {
   radios[i].onclick = function(){
     IDtoSend.value = this.value;
     console.log(this.value);
   }
 }
-// selectedID.addEventListener('change', updateIDtoSend);
 
 adminButton();
