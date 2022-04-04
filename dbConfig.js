00d227495602b4f25676cf888771da671755232e
matthew-sass <matthew.sass35@gmail.com>
2022-04-04 00:24:11 -0700

    Added back admin booking

--- dbConfig.js
+++ dbConfig.js
@@ -5,15 +5,9 @@ if (!isProduction) {
   require('dotenv').config();
 }
 
-<<<<<<< HEAD
 // const connectionString =`postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 const connectionString = 'postgres://postgres:Jojek2020.@localhost/dod' //matts local db
 // const connectionString = 'postgres://postgres:Reset123@localhost/bgcuser'
-=======
-//const connectionString =`postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-// const connectionString = 'postgres://postgres:Jojek2020.@localhost/dod' //matts local db
- const connectionString = 'postgres://postgres:Reset123@localhost/bgcuser'
->>>>>>> e1a78a6e8b3b5fa6c779dae2283742e6b239ae9b
 // connectionString = 'postgres://postgres:root@localhost/bgcusers'
 
 const pool = new Pool({
