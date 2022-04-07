bec0a7cc37e7e15e8c580dca82989dff12ac44e0
Emmii Cui <emmiic@sfu.ca>
2022-04-06 18:56:38 -0700

    minor comments changes

--- public/desksjson.js
+++ public/desksjson.js
@@ -96,22 +96,22 @@ function unhighlight(workstationID){
 }
 
 function floorplan(evt, floor) {
-  // Declare all variables
+  // variables
   var i, tabcontent, tablinks;
 
-  // Get all elements with class="tabcontent" and hide them
+  // hide all elements with class="tabcontent"
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
-  // Get all elements with class="tablinks" and remove the class "active"
+  // elements with class="tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
+  // show current tab, add "active" class to button that opened the tab
   document.getElementById(floor).style.display = "block";
   evt.currentTarget.className += " active";
 }
