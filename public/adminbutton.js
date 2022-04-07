b79e8de1acd664632358fa9c0809ee0dcab361b6
Bhavneet Singh <bsr1@sfu.ca>
2022-04-06 18:52:53 -0700

    svg

--- public/adminbutton.js
+++ public/adminbutton.js
@@ -17,4 +17,58 @@ for (var i = 0; i<radios.length; i++) {
   }
 }
 
-adminButton();
+//adminButton();
+
+
+
+
+function floorplan(evt, floor) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(floor).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+var table = document.getElementsByName('title')
+var tableid = document.getElementsByClassName('workstation');
+for(let i=0;i<table.length;i++){
+  console.log(table[i].value.length);
+}
+
+ let newtable=[];
+for(let i=0;i<table.length;i++){
+  for(let j=0;j<table[i].value.length;j++){
+      if(table[i].value[j]==" "){
+        newtable[i]=table[i].value.substring(0,j)
+        break;
+      }
+  }
+}
+for(let i=0;i<newtable.length;i++){
+  console.log(newtable[i].length);
+}
+
+
+  for(let i=0;i<tableid.length;i++){
+  for(let j=0;j<newtable.length;j++){
+    if(tableid[i].attributes.id.value==newtable[j]){
+      console.log('hello')
+        tableid[i].setAttribute('style', 'fill:green;stroke-width:2;stroke:rgb(0,0,0)')
+    }
+  }
+}  
